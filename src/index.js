67abbe0111eec0b88f9a1c371f93f0ca88c49ca4
DimkaTiM <dt@talentpowered.team>
2022-01-08 03:35:26 +0300

    fix: fix result

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function toReadable (number) {
     if (number == 0) return "zero"
-    letters = (number + "").length == 1 ? `00${number}` : `0${number}`
+    len = (number + "").length
+    letters = len == 1 ? `00${number}` : len == 2 ? `0${number}` : `${number}`
     ent = letters.split("")
-    // console.log(ent)
     
     parts0 = [ 
         "", 
@@ -42,14 +42,7 @@ module.exports = function toReadable (number) {
         'eighty ',
         'ninety ',
     ]
-       
-       
-       
-    // console.log( parts0[ Number(ent[0]) ] )
-    // console.log( doz1[ent[1]] )
-    // console.log( parts0[+ent[2]].split(" ")[0] )
-    let result = parts0[ent[0]] + doz1[ent[1]] + ` ` + parts0[ent[2]].split(" ")[0]
+
+    let result = ( parts0[ent[0]] + doz1[ent[1]] + (ent[1] == 1 ? "" : parts0[ent[2]].split(" ")[0]) ).trim()
     return result;
   }
-  
-//   console.log( toReadable(11) )
